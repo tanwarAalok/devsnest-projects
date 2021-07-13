f84aceee0c29eb45bedb889efacc31aeb3bfe46b
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2021-07-13 21:35:13 +0530

    Add files via upload

---
+++ Daily THA/THA 16/chessboard/src/App.js
@@ -0,0 +1,75 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div class="chessboard">
+      <div class="spot1"></div>
+      <div class="spot1"></div>
+      <div class="spot1"></div>
+      <div class="spot1"></div>
+      <div class="spot1"></div>
+      <div class="spot1"></div>
+      <div class="spot1"></div>
+      <div class="spot1"></div>
+      <div class="spot2"></div>
+      <div class="spot2"></div>
+      <div class="spot2"></div>
+      <div class="spot2"></div>
+      <div class="spot2"></div>
+      <div class="spot2"></div>
+      <div class="spot2"></div>
+      <div class="spot2"></div>
+      <div class="spot3"></div>
+      <div class="spot3"></div>
+      <div class="spot3"></div>
+      <div class="spot3"></div>
+      <div class="spot3"></div>
+      <div class="spot3"></div>
+      <div class="spot3"></div>
+      <div class="spot3"></div>
+      <div class="spot4"></div>
+      <div class="spot4"></div>
+      <div class="spot4"></div>
+      <div class="spot4"></div>
+      <div class="spot4"></div>
+      <div class="spot4"></div>
+      <div class="spot4"></div>
+      <div class="spot4"></div>
+      <div class="spot5"></div>
+      <div class="spot5"></div>
+      <div class="spot5"></div>
+      <div class="spot5"></div>
+      <div class="spot5"></div>
+      <div class="spot5"></div>
+      <div class="spot5"></div>
+      <div class="spot5"></div>
+      <div class="spot6"></div>
+      <div class="spot6"></div>
+      <div class="spot6"></div>
+      <div class="spot6"></div>
+      <div class="spot6"></div>
+      <div class="spot6"></div>
+      <div class="spot6"></div>
+      <div class="spot6"></div>
+      <div class="spot7"></div>
+      <div class="spot7"></div>
+      <div class="spot7"></div>
+      <div class="spot7"></div>
+      <div class="spot7"></div>
+      <div class="spot7"></div>
+      <div class="spot7"></div>
+      <div class="spot7"></div>
+      <div class="spot8"></div>
+      <div class="spot8"></div>
+      <div class="spot8"></div>
+      <div class="spot8"></div>
+      <div class="spot8"></div>
+      <div class="spot8"></div>
+      <div class="spot8"></div>
+      <div class="spot8"></div>
+    </div>
+  )
+}
+
+export default App;
