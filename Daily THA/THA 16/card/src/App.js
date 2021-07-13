0f102bf27f056b0dcb530821162f76f855dc1da3
Aalok Tanwar <aalok.singh2020@vitbhopal.ac.in>
2021-07-13 21:36:33 +0530

    Add files via upload

---
+++ Daily THA/THA 16/card/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="main">
+      <div class="card">
+        <img src="https://th.bing.com/th/id/OIP.IpediaLpieKBR_jS0nmQdAHaFj?w=237&h=180&c=7&o=5&dpr=1.25&pid=1.7"></img>
+        <p>This is card</p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
